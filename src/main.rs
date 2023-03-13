bbdad2af84866fa1d3f944622ef67191c7002807
silite <lemnsilite@gmail.com>
2023-03-13 23:41:45 +0800

    feat: test framework

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,28 @@
 // don’t want to use the normal entry point chain
 #![no_main] // disable all Rust-level entry points
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod test;
+mod serial;
 mod vga_buffer;
 
 use core::panic::PanicInfo;
+
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
 }
 
 /*
@@ -36,31 +44,4 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-// 每个变量都是一个 u32 的整数类型
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-// https://doc.rust-lang.org/1.30.0/book/first-edition/conditional-compilation.html
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
 }
