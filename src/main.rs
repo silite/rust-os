45ea9e36df7fb19f7ba315fb6da836d94672a1cb
silite <lemnsilite@gmail.com>
2023-03-15 14:25:15 +0800

    feat: use x86_64 abstract

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ mod vga_buffer;
 
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
+use x86_64::structures::paging::Translate;
 
 /// This function is called on panic.
 #[cfg(not(test))]
@@ -44,27 +45,29 @@ entry_point!(kernel_main);
 // overwriting the operating system entry point with our own _start function:
 #[no_mangle] // don't mangle the name of this function
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rust_os::memory::active_level_4_table;
-    use x86_64::{structures::paging::PageTable, VirtAddr};
+    use rust_os::memory;
+    use x86_64::VirtAddr;
 
     rust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = mapper.translate_addr(virt);
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
--- src/memory.rs
+++ src/memory.rs
@@ -1,12 +1,10 @@
 use x86_64::{
     registers::control::Cr3,
-    structures::paging::{page_table::FrameError, PageTable},
+    structures::paging::{page_table::FrameError, OffsetPageTable, PageTable},
     PhysAddr, VirtAddr,
 };
 /// 返回一个对活动的4级表的可变引用。
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
-    use x86_64::registers::control::Cr3;
-
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     let (level_4_table_frame, _) = Cr3::read();
     let phys = level_4_table_frame.start_address();
     let virt = physical_memory_offset + phys.as_u64();
@@ -47,4 +45,9 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
 
     // 通过添加页面偏移量来计算物理地址
     Some(frame.start_address() + u64::from(addr.page_offset()))
+}
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
 }
