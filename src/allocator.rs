193eeda08df99a788d5f29fd3aa406fa6e571637
silite <lemnsilite@gmail.com>
2023-03-15 17:32:12 +0800

    feat: global allocator done

--- src/allocator.rs
+++ src/allocator.rs
@@ -9,11 +9,17 @@ use x86_64::{
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
+use linked_list_allocator::LockedHeap;
+
+#[global_allocator]
+static ALLOCATOR: LockedHeap = LockedHeap::empty();
+
 // for map the heap pages to physical memory
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
     frame_allocator: &mut impl FrameAllocator<Size4KiB>,
 ) -> Result<(), MapToError<Size4KiB>> {
+    // map all heap pages to physical frames
     let page_range = {
         let heap_start = VirtAddr::new(HEAP_START as u64);
         let heap_end = heap_start + HEAP_SIZE - 1 as u64;
@@ -29,6 +35,11 @@ pub fn init_heap(
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
         unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() }
     }
+    // end
+
+    // Because the init function already tries to write to the heap memory
+    // must initialize the heap only after mapping the heap pages
+    unsafe { ALLOCATOR.lock().init(HEAP_START, HEAP_SIZE) };
 
     Ok(())
 }
